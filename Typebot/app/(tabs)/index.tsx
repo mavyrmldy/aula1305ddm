536a30f3e960ce344de862ba6684bfceda012178
mavyrmldy x <carlos.franco3@etec.sp.gov.br>
2024-05-13 17:55:59 +0000

    auladdm1305

---
+++ Typebot/app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { Image, Text, StyleSheet, Platform } from 'react-native';
+import { Bubble } from "@typebot.io/react";
+
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Text> Olá mundo</Text>
+      <Bubble typebot="biblioteca" theme={{ button: { backgroundColor: "#7792cf" } }}/>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
